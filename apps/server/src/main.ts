5a4adfa19f03ad400404b0debf071584dbd7869b
周建 <zhoujian@sheencity.com>
2020-12-23 18:10:09 +0800

    feat: 增加打包方式

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,6 +8,10 @@ import { Server } from 'socket.io';
 import * as cors from 'cors';
 import { JsonRpc2 } from '@mengzhidiguo/jsonrpc';
 import { hash } from '@tools/core';
+import { join } from 'path';
+import * as open from 'open';
+import { exit } from 'process';
+import { environment } from './environments/environment';
 
 const app = express();
 
@@ -15,9 +19,8 @@ const socketHttp = createServer(app);
 const io = new Server(socketHttp, { path: '/jsonrpc', cors: { origin: '*' } });
 
 app.use(cors());
-
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to server!' });
+  res.send({ message: 'Ok!' });
 });
 
 function sendResponse(msg: string) {
@@ -38,7 +41,11 @@ io.on('connection', (socket) => {
 });
 
 const port = process.env.port || 3333;
-const server = socketHttp.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+const server = socketHttp.listen(port, async () => {
+  console.log(`Listening at http://localhost:${port}`);
+  if (environment.production) {
+    app.use('/', express.static(join(__dirname, '../', 'shell')));
+    await open(`http://localhost:${port}`).catch(() => exit());
+  }
 });
 server.on('error', console.error);
